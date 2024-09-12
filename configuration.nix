36fe75ca30e8f764dab0a6475fa7690c02e21a0f
jjoyce18 <jjoyce18@gmail.com>
2024-09-12 13:27:00 -0400

    Update configuration.nix
    working config 9/12/24

--- configuration.nix
+++ configuration.nix
@@ -1,111 +1,136 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [
+  imports = [ 
     <nixos-hardware/asus/zephyrus/ga402x/amdgpu>
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [
-    "amd_pstate=passive"
-    "cpufreq.default_governor=powersave"
-    "pcie_aspm.policy=powersupersave"
-  ];
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
+  # Boot configuration
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelParams = lib.mkForce [
+      "amd_pstate=passive"
+      "cpufreq.default_governor=powersave"
+      "pcie_aspm.policy=powersupersave"
+    ];
   };
 
+  # Networking
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  # Nix configuration
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Localization
   time.timeZone = "America/New_York";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
+  # X11 and desktop environment
   services.xserver.enable = true;
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+    defaultSession = "plasma";
   };
-
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasma";
 
+  # Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
+  # System packages
   environment.systemPackages = with pkgs; [
-    kitty
-    kate
-    #auto-cpufreq
-    vim
-    htop
-    btop
-    nmap
-    gimp
-    obsidian
-    google-chrome
-    vscodium
-    flatpak
-    vulkan-tools
-    mesa
-    steam
-    xrdp
-    remmina
-   #blueman
-    discord
-    ticktick
-    git
-    powertop
-    xboxdrv
-    linuxConsoleTools
-    evtest
+    # Terminal utilities
+    vim htop btop nmap git powertop
+    # GUI applications
+    kitty kate gimp obsidian google-chrome vscodium
+    # System tools
+    flatpak vulkan-tools mesa steam xrdp remmina
+    # Gaming and input
+    xboxdrv linuxConsoleTools evtest
+    # Uncomment if needed:
+    # discord ticktick
   ];
 
+  # Environment variables
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
   };
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  # Hardware configuration
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    pulseaudio.enable = false;
+    steam-hardware.enable = true;
+  };
 
+  # Audio
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
     jack.enable = true;
   };
-
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
+  # Firefox
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
   };
 
-  services.flatpak.enable = true;
-  nixpkgs.config.allowUnfree = true;
+  # System services
+  services = {
+    flatpak.enable = true;
+    fstrim.enable = true;
+    thermald.enable = true;
+    power-profiles-daemon.enable = true;
+    # ASUS-specific services
+    supergfxd.enable = true;
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    };
+  };
 
+  # System upgrade
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = false;
+  };
+
+  # User configuration
   users.users.johnny = {
     isNormalUser = true;
     description = "Jonathan Joyce";
@@ -113,52 +138,27 @@
     packages = with pkgs; [ kate ];
   };
 
+  # System version
   system.stateVersion = "24.05";
 
-  # ASUS-specific services
-  services.supergfxd.enable = true;
-  services.asusd = {
-    enable = true;
-    enableUserService = true;
+  # ASUS-specific udev rule
+  services.udev = {
+    extraHwdb = ''
+      evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS:pn*:*
+       KEYBOARD_KEY_ff31007c=f20
+    '';
+    extraRules = ''
+      # Xbox controller rules
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0719", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="02d1", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="02ea", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0b12", MODE="0666"
+    '';
   };
 
-  # ASUS-specific udev rule
-  services.udev.extraHwdb = ''
-    evdev:name::dmi:bvn:bvr:bd:svnASUS:pn:*
-     KEYBOARD_KEY_ff31007c=f20
-  '';
-
-  # Power management configuration
-  services.power-profiles-daemon.enable = true;
+  # Misc
+  nixpkgs.config.allowUnfree = true;
+  fonts.fontconfig.enable = true;
   powerManagement.enable = true;
-
-  # Optional: Enable thermald for better thermal management
-  services.thermald.enable = true;
-  #services.auto-cpufreq.enable = true;
-
-  # New sections for Xbox controller support
-  services.udev.extraRules = ''
-    # Xbox 360 wired controller
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", MODE="0666"
-    # Xbox 360 wireless controller
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0719", MODE="0666"
-    # Xbox One controller
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="02d1", MODE="0666"
-    # Xbox One S controller
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="02ea", MODE="0666"
-    # Xbox Series X|S controller
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0b12", MODE="0666"
-  '';
-
-  # Enable Steam hardware support
-  hardware.steam-hardware.enable = true;
-
-  # Optional: Enable xboxdrv service (commented out by default)
-  # services.xboxdrv.enable = true;
-
-  # Enable TRIM for SSDs
-  services.fstrim.enable = true;
-
-  # Enable Gamemode
-  programs.gamemode.enable = true;
 }
